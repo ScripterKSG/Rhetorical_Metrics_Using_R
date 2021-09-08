fbe40cc7fdfa62eb584d6f943742d6d78708ac97
ScripterKSG <jasonn1210@gmail.com>
2021-09-08 01:57:00 -0400

    Finished the rest of the HW2 and better understood the function of the code

--- homework2.R
+++ homework2.R
@@ -20,17 +20,24 @@ data <- map(files,function(x) read_csv(x))
 # - is also a list of 11
 gop_data <- map2(files,data, function(x,y) cbind(x,y))
 
-#defines a variable gop_df which stores text data from speakers such as
+#defines a variable gop_df which stores text and date data from speakers such as
 #Trump, Paul and Carson
 gop_df <- do.call(rbind,gop_data)
 
 
+#renames the variable x in gop_df to date
 names(gop_df)[1] <- "date"
 
+#df1 takes the data from gop_df and adds on 2 columns, one which points out the 
+#speaker of each line of text and another which records the text length
 df1 <- gop_df %>% 
   separate(date,"date",sep = "\\.") %>% 
   separate(text, "speaker", sep = ":", remove = FALSE) %>% 
   mutate(text_length = nchar(text))
+
+#df2 takes the data from df1 and uses it to find and list the speakers, how
+#many times they each took a turn to talk, their total text length (or total
+#number of characters spoken) and their average text length per turn
 df2 <- df1 %>% 
   group_by(speaker) %>% 
   summarise(talking_turns = n(), 
