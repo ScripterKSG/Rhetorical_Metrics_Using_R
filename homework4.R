56e08b38f7aace450f8f7b8d14e54a16a9ffe762
ScripterKSG <jasonn1210@gmail.com>
2021-09-17 18:10:48 -0400

    Starting work on HW4, mainly utilizing code from HW3 and lexdiv workshop

---
+++ homework4.R
@@ -0,0 +1,40 @@
+#Homework 3
+
+#imports packages from the tidyverse and quanteda
+library(tidyverse)
+library(quanteda)
+library(quanteda.textstats)
+library(dabestr)
+
+#sets working directory inside home/datasets/gop_frags
+setwd("~/datasets/gop_frags/")
+
+#defines a variable files which stores a list of the files in the working
+#directory since gop_frags is a folder with several csv files in it
+files <- list.files()
+
+#gets the data from files and uses it to create gop_data
+#and then gop_df then the date and text is organized in columns
+data <- map(files,function(x) read_csv(x))
+gop_data <- map2(files,data, function(x,y) cbind(x,y))
+gop_df <- do.call(rbind,gop_data)
+names(gop_df)[1] <- "date"
+
+df1 <- gop_df %>% 
+  separate(date,"date",sep = "\\.") %>% 
+  separate(text, "speaker", sep = ":", remove = FALSE) %>% 
+  mutate(text_length = nchar(text))
+
+#add readability metric columns to df1 which analyzes each
+#speaking turn in several different metrics
+df1 <- df1 %>% 
+  bind_cols(textstat_readability(.$text,measure = c("Flesch")))
+
+#removes unneeded columns of date, document and text length
+#df2 is the avg speaking complexity of each speaking turn
+df2 <- df1[ -c(1,4,5)]
+
+
+
+
+  
