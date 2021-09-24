8fa2a316f243b2c856e0de31f65cf4d4725ebe68
ScripterKSG <jasonn1210@gmail.com>
2021-09-24 18:01:38 -0400

    changed read.cvs to read_csv,changed formatting of the charts from the displayed combined_data. Added commented code about displaying wordfrequency

--- homework5.R
+++ homework5.R
@@ -1,9 +1,9 @@
-#imports packages from the tidyverse and quanteda
+#imports packages from the tidyverse and tidytext
 library(tidyverse)
 library(tidytext)
 
 #reads debate data, changes column names to have date and speaker
-data <- read.csv("~/datasets/gop_debates.csv", stringsAsFactors=FALSE)
+data <- read_csv("~/datasets/gop_debates.csv")
 names(data)[1] <- "date"
 names(data)[3] <- "speaker"
 
@@ -12,6 +12,7 @@ data_word_n <- data %>%
   unnest_tokens(word, text) %>%
   anti_join(stop_words) %>%
   count(date, word, sort = TRUE)
+
 
 #total number of words said during each date (aka each debate)
 total_words <- data_word_n %>% 
@@ -36,6 +37,19 @@ combined_data  %>%
   ggplot(aes(word, tf_idf, fill = date)) +
   geom_col(show.legend = FALSE) +
   labs(x = NULL, y = "tf-idf") +
-  facet_wrap(~date, ncol = 2, scales = "free") +
+  facet_wrap(~date, ncol = 3, scales = "free") +
   coord_flip()
 
+
+# #stores most common words said during each date
+# word_freq <- data_word_n %>%
+#   mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+#   group_by(date) %>% 
+#   slice(1:10) %>% 
+#   ungroup() %>%
+#   ggplot(aes(x = reorder(word, n), y = n, fill=date)) +
+#   geom_col() +
+#   xlab(NULL) +
+#   coord_flip()+
+#   # Separate charts by date using facet_wrap()
+#   facet_wrap(~date, ncol = 3, scales = "free_y")
