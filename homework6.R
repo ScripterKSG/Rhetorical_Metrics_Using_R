e115179966b75cbd0a090751bc9e570159d8f9b1
ScripterKSG <jasonn1210@gmail.com>
2021-10-03 21:26:30 -0400

    Cleanned up code to more efficiently display framegrams for Trump, Rubio and Cruz regarding immigration. Added comments

--- homework6.R
+++ homework6.R
@@ -2,62 +2,36 @@
 library(tidyverse)
 library(tidytext)
 library(textstem)
-library(gridExtra)
 
 # Load data
 data <- read_csv("~/datasets/gop_debates.csv")
 
+# find and stores stem of immigration and lemmatized immgration
 stem_strings("immigration")
 lemmatize_strings("immigration")
 immigration_terms <- "immigr|immigration"
 
+# deals with removing stop words
 paste(stop_words$word, collapse = "|")
 stop_words_bounded <- paste0("\\b", stop_words$word, "\\b", collapse = "|")
-  
-data_TRUMP <- data %>%
-  filter(who == "TRUMP")
 
-data_CRUZ <- data %>%
-  filter(who == "CRUZ")
+# filters out data to only have trump, cruz and rubio's speaking turns
+data_Speakers <- data %>%
+  filter(who == "TRUMP" | who == "CRUZ" | who == "RUBIO") %>%
+  group_by(who, text)
 
-data_RUBIO <- data %>%
-  filter(who == "RUBIO")
-
-plot_TRUMP <- data_TRUMP %>%
+# graphs lemmaitzed framegrams for trump, cruz, rubio
+data_Speakers %>%
   unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
   count(trigram, sort = TRUE) %>%
   filter(str_detect(trigram,immigration_terms)) %>% 
   filter(str_count(trigram,stop_words_bounded) < 1) %>% 
   mutate(trigram = reorder(trigram, n)) %>%
+  group_by(who) %>%
   slice(1:10) %>% # top 10 
+  ungroup() %>%
   ggplot(aes(x=trigram, y=n)) +
   geom_col() +
   xlab(NULL) +
+  facet_wrap(~who, ncol = 3, scales = "free") +
   coord_flip()
-
-plot_CRUZ <- data_CRUZ %>%
-  unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
-  count(trigram, sort = TRUE) %>%
-  filter(str_detect(trigram,immigration_terms)) %>% 
-  filter(str_count(trigram,stop_words_bounded) < 1) %>% 
-  mutate(trigram = reorder(trigram, n)) %>%
-  slice(1:10) %>% # top 10 
-  ggplot(aes(x=trigram, y=n)) +
-  geom_col() +
-  xlab(NULL) +
-  coord_flip()
-
-plot_RUBIO <- data_RUBIO %>%
-  unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
-  count(trigram, sort = TRUE) %>%
-  filter(str_detect(trigram,immigration_terms)) %>% 
-  filter(str_count(trigram,stop_words_bounded) < 1) %>% 
-  mutate(trigram = reorder(trigram, n)) %>%
-  slice(1:10) %>% # top 10 
-  ggplot(aes(x=trigram, y=n)) +
-  geom_col() +
-  xlab(NULL) +
-  coord_flip()
-
-
-grid.arrange(plot_TRUMP, plot_CRUZ, plot_RUBIO, ncol=3)
