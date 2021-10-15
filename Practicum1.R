9184878d5adace9f7e971ee6cb97c9294653d036
ScripterKSG <jasonn1210@gmail.com>
2021-10-15 13:23:57 -0900

    Take one on Practicum, doing stem analyis for both pos and neg bing dictionary

---
+++ Practicum1.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(tidytext)
+library(textstem)
+library(fuzzyjoin)
+library(dabestr)
+
+#import data, add row IDs to each text, currently using stem analysis
+data <- read_csv("~/nguyen_jason/spam_ham.csv")%>% mutate(id=row.names(.))
+bing_pos_stem <- get_sentiments("bing") %>% filter(sentiment=="positive") %>% mutate(word=stem_words(word))
+bing_neg_stem <- get_sentiments("bing") %>% filter(sentiment=="negative") %>% mutate(word=stem_words(word))
+
+#bing_neg_lem <- get_sentiments("bing") %>% filter(sentiment=="negative") %>% mutate(word=lemmatize_words(word))
+#bing_pog_lem <- get_sentiments("bing") %>% filter(sentiment=="positive") %>% mutate(word=lemmatize_words(word))
+
+pos_analysis <- data %>% 
+  unnest_tokens(word, Message, token = "words") %>% 
+  mutate(word=stem_words(word)) %>% 
+  inner_join(bing_pos_stem) %>% 
+  add_count(id) %>% 
+  select(id,Type,n) %>% 
+  unique()
+
+neg_analysis <- data %>% 
+  unnest_tokens(word, Message, token = "words") %>% 
+  mutate(word=stem_words(word)) %>% 
+  inner_join(bing_neg_stem) %>% 
+  add_count(id) %>% 
+  select(id,Type,n) %>% 
+  unique()
+
+#Pos average
+pos_analysis %>% group_by(Type) %>% 
+  summarise(ave_fear = mean(n))
+
+#Neg average
+neg_analysis %>% group_by(Type) %>% 
+  summarise(ave_fear = mean(n))
