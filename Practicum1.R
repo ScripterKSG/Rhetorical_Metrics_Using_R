af730a8d123aebc8ef47ddfb420637e8da833307
ScripterKSG <jasonn1210@gmail.com>
2021-10-17 22:58:03 -0900

    Added comments, cleaned up code.

--- Practicum1.R
+++ Practicum1.R
@@ -9,9 +9,12 @@ data <- read_csv("~/nguyen_jason/spam_ham.csv")%>% mutate(id=row.names(.))
 bing_pos_stem <- get_sentiments("bing") %>% filter(sentiment=="positive") %>% mutate(word=stem_words(word))
 bing_neg_stem <- get_sentiments("bing") %>% filter(sentiment=="negative") %>% mutate(word=stem_words(word))
 
-#bing_neg_lem <- get_sentiments("bing") %>% filter(sentiment=="negative") %>% mutate(word=lemmatize_words(word))
-#bing_pog_lem <- get_sentiments("bing") %>% filter(sentiment=="positive") %>% mutate(word=lemmatize_words(word))
 
+#bing_neg_lem <- get_sentiments("bing") %>% filter(sentiment=="negative") %>% mutate(word=lemmatize_words(word))
+#bing_pos_lem <- get_sentiments("bing") %>% filter(sentiment=="positive") %>% mutate(word=lemmatize_words(word))
+
+#creates 2 data frames, one which contains text lines with positive words and the number of said words
+#and one which contains text lines with negative words and the number of said ones
 pos_analysis <- data %>% 
   unnest_tokens(word, Message, token = "words") %>% 
   mutate(word=stem_words(word)) %>% 
@@ -28,14 +31,16 @@ neg_analysis <- data %>%
   select(id,Type,n) %>% 
   unique()
 
+
 #Pos average
 pos_analysis %>% group_by(Type) %>% 
-  summarise(ave_fear = mean(n))
+  summarise(ave_pos = mean(n))
 
 #Neg average
 neg_analysis %>% group_by(Type) %>% 
-  summarise(ave_fear = mean(n))
+  summarise(ave_neg = mean(n))
 
+#dabest magic
 pos_dabest <- pos_analysis %>% 
   dabest(x = Type,
          y= n,
@@ -49,6 +54,7 @@ neg_dabest <- neg_analysis %>%
          paired = FALSE)
 
 
+#calculates CI and plots data
 pos_dabest %>% mean_diff()
 neg_dabest %>% mean_diff()
 
