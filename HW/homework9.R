481462c0793cebb58c9c24bb5eebd7414e02cf55
ScripterKSG <jasonn1210@gmail.com>
2021-11-08 13:09:36 -1000

    Added comments, successfully ran machine test

--- HW/homework9.R
+++ HW/homework9.R
@@ -6,14 +6,16 @@ library(tidytext)
 library(caret)
 library(pROC)
 
-#get dataset and label yes/no for spam
+#get dataset,
+#add unique row IDs
+#and label yes/no for spam
 data <- read_csv("~/datasets/spam_ham.csv") %>% 
   select(Type, Message) %>%
-  mutate(spam = ifelse(Type=="spam","yes","no")) %>%
   unique()
 
-#add unique row IDs
 data$ID <- seq.int(nrow(data))
+
+data_labels <- data %>% mutate(spam = ifelse(Type=="spam","yes","no")) %>% select(ID,spam)
 
 
 data_counts <- map_df(1:2, # map iterates over a list, in this case the list is 1:2
@@ -38,7 +40,7 @@ data_engineered <- data_dtm %>%
   as.matrix() %>% 
   as.data.frame() %>% 
   mutate(ID = as.numeric(dimnames(data_dtm)[[1]])) %>% 
-  right_join(data) %>% 
+  right_join(data_labels) %>% 
   filter(complete.cases(.))  
 
 
@@ -49,3 +51,28 @@ test_set <- data_engineered %>% anti_join(training_set, by="ID") %>% select(-ID)
 
 # Create training set with No ID
 training_set_no_id <- training_set %>% select(-ID)
+
+
+
+fitControl <- trainControl(
+  method = "repeatedcv", # set method to repeated cross-validation 
+  number = 5, # Divide data into 5 sub-sets
+  repeats = 5) # Repeat the cross-validation 5 times
+
+# Train SVM model 
+svm_mod <- train(spam ~ ., # predict spam based on the remaining data 
+                 data = training_set_no_id, # identify the training data set 
+                 method = "svmLinearWeights2", # specify the algorithm to be used 
+                 trControl = fitControl, # import the train control parameters 
+                 tuneGrid = data.frame(cost = 1, 
+                                       Loss = 0, 
+                                       weight = 1)) # provide tuning parameters (basically ignore these)
+
+# Predict against test set 
+svm_pred <- test_set %>% 
+  select(-spam) %>% # get rid of the label so it's a fair prediction 
+  predict(svm_mod, newdata = .) # use the pre-trained model (svm_mod) to predict the label for the test set data
+
+
+# Get confusion matrix, agreement, and accuracy values 
+confusionMatrix(svm_pred,as.factor(test_set$spam))
