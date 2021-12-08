7e7b16c0db76895742743400dbd86ebdb35eb0c8
ScripterKSG <jasonn1210@gmail.com>
2021-12-08 08:03:27 -1000

    Project Draft 1

--- HW/homework10.R
+++ HW/homework10.R
@@ -1,38 +1,113 @@
-install.packages("devtools")
-devtools::install_github("mkearney/rtweet")
 
 library(rtweet)
 library(httr)
+library(tidyverse)
+library(caret)
+
+### First part of code demonstrates twitter scrapping, not necessary to run
+### as we already saved a csv of a dataset from earlier
 
 ### Put project name, consumer and secret keys, access token and secret
-### Tokens have been stripped away in this instance but are saved on my end
 
-#appname <- ""
-#key <- ""
-#secret <- ""
-#atoken <- ""
-#asecret<- ""
+appname <- "RHE330_Project"
+key <- "7c15tU59YLsycvzwywlVDCLVw"
+secret <- "goTfoFOMIfySh7S0civ0B6ezrY7MqYYQNLYjgdW3IDN0Luum4p"
+atoken <- "1241260280228782081-7UFpZhmkjcr9NNO4IXJZHqgj4JbueJ"
+asecret<- "Ny7ePD8gOucoCS1qzlQTGmGHDqegBqLTAWUOdhp1XmqcC"
 
 ### create token
 
-#twitter_token <- create_token(
-#  app = appname,
-#  consumer_key = key,
-#  consumer_secret = secret,
-#  access_token = atoken,
-#  access_secret = asecret)
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = key,
+  consumer_secret = secret,
+  access_token = atoken,
+  access_secret = asecret)
+
+## search for tweets involving america and europe
+rt <- search_tweets("america", n = 500)
+rt2 <- search_tweets("europe", n = 500)
+
+# select needed columns, mutate and rename full_text col to be a label
+rt <-  rt %>% select(text, full_text) %>% mutate(full_text = "yes")
+names(rt)[2] <- "usa"
+rt2 <- rt2 %>% select(text, full_text) %>% mutate(full_text = "no")
+names(rt2)[2] <- "usa"
+
+#create our dataset
+data <- rbind(rt, rt2)
+data$ID <- seq.int(nrow(data))
+
+#save dataset
+write.csv(data,"~/nguyen_jason/us_eu_tweets.csv", row.names = FALSE)
 
 
-### Save token in RDS< read token
-saveRDS(twitter_token, file = "API")
 
-readRDS(file = "API")
 
-## search for 1000 tweets using the hololive hashtag
-rt <- search_tweets("hololive", n = 1000)
 
-## preview tweets data
-rt
+####ACTUAL SCRIPT BELOW
+data <- read_csv("~/nguyen_jason/us_eu_tweets.csv")
+data_labels <- data %>% select(ID, usa)
 
-## preview users data
-users_data(rt)
+# Tokenize by term and bigram and get TFs by status_id
+data_counts <- map_df(1:2, # map iterates over a list, in this case the list is 1:2
+                      ~ unnest_tokens(data, word, text, 
+                                      token = "ngrams", n = .x)) %>% # .x receives the values for the list
+  anti_join(stop_words, by = "word") %>%
+  count(ID, word, sort = TRUE)
+
+# Get only terms and bigrams with a TF > 10 
+words_10 <- data_counts %>%
+  group_by(word) %>%
+  summarise(n = n()) %>% 
+  filter(n >= 10) %>%
+  select(word) %>%
+  na.omit()
+
+# Create a document term matrix (DTM)
+data_dtm <- data_counts %>%
+  right_join(words_10, by = "word") %>%
+  bind_tf_idf(word, ID, n) %>%
+  cast_dtm(ID, word, tf_idf)
+
+# Add labels to DTM and filter for complete cases
+data_engineered <- data_dtm %>% 
+  as.matrix() %>% 
+  as.data.frame() %>% 
+  mutate(ID = as.numeric(dimnames(data_dtm)[[1]])) %>% 
+  right_join(data_labels) %>% 
+  filter(complete.cases(.))
+
+# Create training set based on an 80/20 split
+training_set <- data_engineered %>% slice_sample(prop =.8)
+
+# Create testing set
+test_set <- data_engineered %>% anti_join(training_set, by="ID") %>% select(-ID)
+
+# Create training set with No ID
+training_set_no_id <- training_set %>% select(-ID)
+
+
+# Set train control to repeated cross-validation 
+fitControl <- trainControl(
+  method = "repeatedcv", # set method to repeated cross-validation 
+  number = 5, # Divide data into 5 sub-sets
+  repeats = 5) # Repeat the cross-validation 5 times
+
+# Train SVM model 
+svm_mod <- train(usa ~ ., # predict china based on the remaining data 
+                 data = training_set_no_id, # identify the training data set 
+                 method = "svmLinearWeights2", # specify the algorithm to be used 
+                 trControl = fitControl, # import the train control parameters 
+                 tuneGrid = data.frame(cost = 1, 
+                                       Loss = 0, 
+                                       weight = 1)) # provide tuning parameters (basically ignore these)
+
+# Predict against test set 
+svm_pred <- test_set %>% 
+  select(-usa) %>% # get rid of the label so it's a fair prediction 
+  predict(svm_mod, newdata = .) # use the pre-trained model (svm_mod) to predict the label for the test set data
+
+
+# Get confusion matrix, agreement, and accuracy values 
+confusionMatrix(svm_pred,as.factor(test_set$usa))
